37a1241477e79e875f6c711888f562478ac7c887
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-08 04:29:13 +0300

    add game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <img class="logo animate__animated animate__backInRight" src="./assets/logo.png" alt="Vue logo">
+    <h1 class="h1 animate__animated animate__backInLeft">Remember Me</h1>
+    <p>Remember and open all green cells</p>
+    <v-boards></v-boards>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import VBoards from "@/components/VueBoard.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VBoards
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.app {
+  font-family: Helvetica,sans-serif;
+  margin-top: 40px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.logo {
+  width: 100px;
 }
 </style>
